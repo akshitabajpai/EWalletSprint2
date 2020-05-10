b09f134c40239c4de10bdcebeeba0dba18369e2e
DELL <DELL@DESKTOP-FRIMMDM>
2020-05-10 17:55:44 +0530

    second commit

--- E-Wallet/src/main/java/com/capgemini/ewallet/controller/TransactionController.java
+++ E-Wallet/src/main/java/com/capgemini/ewallet/controller/TransactionController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.capgemini.ewallet.entity.WalletTransactions;
 import com.capgemini.ewallet.service.TransactionServiceImpl;
 
 @RestController
+@RequestMapping("/transaction")
+
 public class TransactionController {
 	
 	@Autowired 
